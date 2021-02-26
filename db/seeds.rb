19e95d8f2044b6769f374a992547636de47cf933
samuel schwob <samuelschwob@gmail.com>
2021-02-26 11:53:11 +0100

    Clean scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Cleaning database..."
-# Cocktail.destroy_all
+Cocktail.destroy_all
 Ingredient.destroy_all
 
-# puts "Add cocktails"
-# Cocktail.create!(name: "Bikini", image: "cocktail_bikini.jpg")
-# Cocktail.create!(name: "Blue Lagon", image: "cocktail_blue-lagon.jpg")
-# Cocktail.create!(name: "Frozen Margarita", image: "cocktail_frozen-margarita.jpg")
-# Cocktail.create!(name: "Green Fury", image: "cocktail_green-fury.jpg")
-# Cocktail.create!(name: "Mangalicious", image: "cocktail_mangalicious.jpg")
-# Cocktail.create!(name: "Virgin Colada", image: "cocktail_virgin-colada.jpg")
-# puts "cocktails created"
+puts "Add cocktails"
+Cocktail.create!(name: "Bikini", image: "cocktail_bikini.jpg")
+Cocktail.create!(name: "Blue Lagon", image: "cocktail_blue-lagon.jpg")
+Cocktail.create!(name: "Frozen Margarita", image: "cocktail_frozen-margarita.jpg")
+Cocktail.create!(name: "Green Fury", image: "cocktail_green-fury.jpg")
+Cocktail.create!(name: "Mangalicious", image: "cocktail_mangalicious.jpg")
+Cocktail.create!(name: "Virgin Colada", image: "cocktail_virgin-colada.jpg")
+puts "cocktails created"
 
 puts "Add ingrdedients"
 ingredients = [["Menthe", "mint"], ["Citron Vert", "green-lime"], ["Citron Jaune", "lemon"], ["Pamplemousse", "grapefruit"], ["Blanc d'Oeuf", "egg"], ["Fraises", "strawberry"], ["Jus de Citron Vert", "green-lemon-juice"],
