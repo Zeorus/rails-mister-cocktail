ff3255fd428d06ae38d9ca6439937c5e97fefa2f
samuel schwob <samuelschwob@gmail.com>
2021-02-28 08:14:54 +0100

    Rollback and remove Editable Column for Dose Model

--- db/migrate/20210228065125_add_column_editable_for_dose_model.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnEditableForDoseModel < ActiveRecord::Migration[6.1]
-  def change
-    add_column :doses, :editable, :boolean, default: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_065125) do
+ActiveRecord::Schema.define(version: 2021_02_28_065109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 2021_02_28_065125) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "quantity"
-    t.boolean "editable", default: true
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
